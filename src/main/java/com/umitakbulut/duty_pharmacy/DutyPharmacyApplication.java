572c3bf88b19ac029397c8adef1a245a0173ff14
deneme <com.deneme>
2025-05-10 19:01:01 +0300

    Initial commit

---
+++ src/main/java/com/umitakbulut/duty_pharmacy/DutyPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.umitakbulut.duty_pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutyPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutyPharmacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umitakbulut/duty_pharmacy/DutyPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umitakbulut.duty_pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DutyPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
